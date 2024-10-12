67a992f2ecd962b7baea516e4233081880fd5d83
Didier Krux <didier@krux.co>
2024-10-12 21:19:00 +0100

    clean config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,3 @@
-import { cookieStorage, createStorage } from '@wagmi/core'
-import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { mainnet, arbitrum } from '@reown/appkit/networks'
-
 export const eventId = 1
 
 export const adminWallets = [
@@ -19,17 +15,3 @@ export const potionUrl = 'https://potion.banklessacademy.com/table?id=37a9e401c5
 if (!projectId) {
   throw new Error('Project ID is not defined')
 }
-
-export const networks = [mainnet, arbitrum]
-
-// Set up the Wagmi Adapter (Config)
-export const wagmiAdapter = new WagmiAdapter({
-  storage: createStorage({
-    storage: cookieStorage
-  }),
-  ssr: true,
-  projectId,
-  networks,
-})
-
-export const config = wagmiAdapter.wagmiConfig
