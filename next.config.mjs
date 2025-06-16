3a9264d46cf0eab7128ed49501be7822307d2618
Didier Krux <didier@krux.co>
2025-06-16 20:44:39 +0100

    fix build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // Handle WalletConnect worker files
+    config.module.rules.push({
+      test: /HeartbeatWorker\.js$/,
+      use: {
+        loader: 'null-loader'
+      }
+    });
+
+    return config;
+  },
 }
 
 export default nextConfig
